2ff6bb11898e8719d5e1fdbf70ada3bfe57c6aea
kiptook7070 <kiptook2020@gmail.com>
2024-03-11 22:48:31 +0300

    virtua threads in java 21 with springboot 3.2

---
+++ src/main/java/com/kiptoo/virtualThreads/VirtualThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.kiptoo.virtualThreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualThreadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiptoo/virtualThreads/VirtualThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiptoo.virtualThreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
